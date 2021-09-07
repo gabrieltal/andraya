a593ede50559454da8e87a3fd604069bd0082767
Gabriel <gabrielvt6@gmail.com>
2021-09-06 21:58:50 -0700

    Add some events to the history (not finalized at all)

--- src/library/history.js
+++ src/library/history.js
@@ -8,6 +8,22 @@ const HISTORY = [
     description: 'We had first date. We watch Mission: Impossible - Fallout. The air is electric.',
   },
   {
+    date: '08/13/2018',
+    description: 'A date to the LACMA museum. It is a memorable one in our relationship and the connection is strong.'
+  },
+  {
+    date: '11/11/2018',
+    description: 'We have a trip to the Grand Canyon, our first major trip',
+  },
+  {
+    date: '02/14/2019',
+    description: 'Our first valentine\'s together.',
+  },
+  {
+    date: '06/01/2019',
+    description: 'We move in together, joining our family together.'
+  },
+  {
     date: '08/07/2021',
     description: 'Gabriel proposes to Andraya at Griffith Observatory. Our love is written in the stars.',
   },
