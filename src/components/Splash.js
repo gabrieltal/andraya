3612e4eaa7c5dd20b5b1eb5dcbe1d91d8c22782e
Gabriel <gabrielvt6@gmail.com>
2021-09-18 20:10:34 -0700

    Add spacing to bottom of page and allow scroll

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -10,7 +10,7 @@ export default class Splash extends Component {
           <img src={moonImage} height="128px" width="176px" alt="moon"/>
           <h1 className="mt-3 mb-1">Andraya and Gabriel</h1>
           <p className="mt-1 mb-3">Date TBD</p>
-          <div className="main-image">
+          <div className="main-image mb-5">
             <img src={coupleImage} className="couple-image" alt="Andraya and Gabriel on one of our first dates to LACMA" />
           </div>
         </main>
