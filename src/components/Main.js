13e1ecec9458cb69124902620b282a861a786e24
Gabriel <gabrielvt6@gmail.com>
2021-09-18 19:32:02 -0700

    Basic homepage

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,28 +1,11 @@
 import React, { Component } from 'react';
-import Timeline from './Timeline';
 import Splash from './Splash';
 
 export default class Main extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = { showTimeline: false };
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick() {
-    this.setState(prevState => ({
-      showTimeline: true
-    }));
-  }
-
   render() {
-    const timeline = <Timeline />;
-    const splash = <Splash handleClick={this.handleClick}/>;
-
     return (
       <div>
-        {this.state.showTimeline === true ? timeline : splash}
+        <Splash/>
       </div>
     );
   }
--- src/components/Splash.js
+++ src/components/Splash.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
+import moonImage from '../assets/images/moon-sun.png';
+import coupleImage from '../assets/images/cute-couple.JPG';
 
 export default class Splash extends Component {
   render() {
     return (
-      <div className="splash" onClick={this.props.handleClick}>
+      <div className="splash">
         <header>
-          <h1>Andraya and Gabriel</h1>
-          <p>August 07, 2022</p>
+          <img src={moonImage} height="128px" width="176px" />
+          <h1 class="my-3">Andraya and Gabriel</h1>
+          <img src={coupleImage} height="504px" width="384px" />
         </header>
       </div>
     );
